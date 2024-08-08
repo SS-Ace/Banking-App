b3a46c40fb21b3f252d1786d2baf7a4d41bf3fd5
SudhanshuSharma <SudhanshuSharma@IBM-5YNNH24.in.ibm.com>
2024-08-08 16:21:58 +0530

    Annotation + Database update

--- src/main/java/com/ibm/bankingapp/repo/TransactionRepository.java
+++ src/main/java/com/ibm/bankingapp/repo/TransactionRepository.java
@@ -1,9 +1,11 @@
 package com.ibm.bankingapp.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ibm.bankingapp.model.Transaction;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
 
 }
