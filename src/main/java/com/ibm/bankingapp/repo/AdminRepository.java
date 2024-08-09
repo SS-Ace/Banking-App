2b4400f89e485e1f677e3e8684ba56d743791c57
Sudhanshu Sharma <sudhanshu.sharma@ibm.com>
2024-08-09 10:42:37 +0530

    admin repo

--- src/main/java/com/ibm/bankingapp/repo/AdminRepository.java
+++ src/main/java/com/ibm/bankingapp/repo/AdminRepository.java
@@ -1,5 +1,7 @@
 package com.ibm.bankingapp.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.ibm.bankingapp.model.Admin;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long>{
-
+	List<Admin> findByUsernameAndPassword(String username, String password);
+	
 }
