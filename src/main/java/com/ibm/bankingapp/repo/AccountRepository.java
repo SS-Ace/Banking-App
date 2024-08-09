b48bff5ab2234c3dfb7d0fe2d1d5ef41166aeda6
Sudhanshu Sharma <sudhanshu.sharma@ibm.com>
2024-08-09 11:49:00 +0530

    account repo customer

--- src/main/java/com/ibm/bankingapp/repo/AccountRepository.java
+++ src/main/java/com/ibm/bankingapp/repo/AccountRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ibm.bankingapp.model.Account;
+import com.ibm.bankingapp.model.Customer;
+
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	List<Account> findByAccountNumber(Long accountNumber);
-	List<Account> findAll();
 	List<Account> findBalanceByAccountNumber(Long accountNumber);
+	List<Account> findByCustomer(Customer customer);
+	void deleteByAccountNumber(Long accountNumber);
 }
