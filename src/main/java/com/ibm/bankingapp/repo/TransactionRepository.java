a033bba61159744bea5c29d3fdb6bd88669833fa
Sudhanshu Sharma <sudhanshu.sharma@ibm.com>
2024-08-09 10:40:45 +0530

    repository update in transaction and admin

--- src/main/java/com/ibm/bankingapp/repo/TransactionRepository.java
+++ src/main/java/com/ibm/bankingapp/repo/TransactionRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import com.ibm.bankingapp.model.Transaction;
 import java.time.LocalDateTime;
+import com.ibm.bankingapp.model.Account;
+
 
 
 @Repository
@@ -15,6 +17,9 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long>{
 	List<Transaction> findAmountByTransactionId(Long transactionId);
 	List<Transaction> findTransactionDateByTransactionId(Long transactionId);
 	List<Transaction> findByTransactionDate(LocalDateTime transactionDate);
+	List<Transaction> findBySourceAccount(Account sourceAccount);
+	List<Transaction> findByDestinationAccount(Account destinationAccount);
+	List<Transaction> findByTransactionDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 	void deleteByTransactionId(Long transactionId);
 	
 }
